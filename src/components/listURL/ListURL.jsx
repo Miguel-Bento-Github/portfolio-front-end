16d7786ef7e318956b1229224df1e29af461a6bc
Miguel Bento <miguel.bento@eurail.com>
2020-01-13 10:25:41 +0100

    #2 - Fix font import.
    - Use font Overpass. Previous font face usage was incorrect.

--- src/components/listURL/ListURL.jsx
+++ src/components/listURL/ListURL.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const ListURL = ({ exact, to, name, close }) => {
+  console.log(to);
+
   return (
     <li>
       <NavLink onClick={close} className="nav-link" exact={exact} to={to}>
