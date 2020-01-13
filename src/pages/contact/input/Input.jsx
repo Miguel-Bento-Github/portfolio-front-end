6242ea36df6c111d207e2c068c463f462fa24ae5
Miguel Bento <miguel.bento@eurail.com>
2020-01-13 19:35:55 +0100

    #6 - Align error messages.

--- src/pages/contact/input/Input.jsx
+++ src/pages/contact/input/Input.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Input = ({ handleChange, pHolder }) => {
+  const [value, setValue] = useState("");
+
   return (
     <input
-      onChange={e => handleChange(e)}
+      onChange={e => {
+        handleChange(e);
+        setValue(e.target.value);
+      }}
       className={`input  input-${pHolder}`}
       placeholder={pHolder}
       type={pHolder === "email" ? "email" : "text"}
+      maxLength={30}
+      value={value}
     />
   );
 };
