39b43a295fcfab6bc55cea7ee62ee3b01b920641
Miguel Bento <miguel.bento@eurail.com>
2020-01-13 19:12:04 +0100

    #5 - New favicon. Fix 404 background.

--- src/pages/skills/Skills.jsx
+++ src/pages/skills/Skills.jsx
@@ -1,14 +1,13 @@
-import React, { Component } from "react";
+import React, { memo } from "react";
 import IconsList from "./iconsList/IconsList";
 import "./skills.scss";
 
-export default class Skills extends Component {
-  render() {
-    return (
-      <section className="skills secondBg section blur">
-        <h1 className="section-header skills-title">Skills</h1>
-        <IconsList />
-      </section>
-    );
-  }
-}
+const Skills = () => {
+  return (
+    <section className="skills secondBg section blur">
+      <h1 className="section-header skills-title">Skills</h1>
+      <IconsList />
+    </section>
+  );
+};
+export default memo(Skills);
