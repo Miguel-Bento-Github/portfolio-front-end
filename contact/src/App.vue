c77370c59308ee266a8e30732534884a32510823
Miguel Bento <miguel.angelo.f.bento@gmail.com>
2021-10-22 16:10:07 +0200

    Add app scaffold
    - Vue 3
    - TS
    - Cypress
    - Sass
    - PWA

---
+++ contact/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Miguel Bento</h1>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
